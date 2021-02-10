77d8fd43f5193af0239089d21558aa4644ec6fde
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-10 12:51:47 +0530

    First Java Program With Explination

---
+++ Hello1.java
@@ -0,0 +1,60 @@
+/*
+
+Description      :  First Java Program With Explination 
+Function Date    :  10 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  String
+
+*/
+
+import java.io.*;
+
+class Demo
+{
+    public static void main(String arg[])
+    {
+        System.out.println("Hello Im Prasad Dangare And This Is My First Program In Java");
+    }
+}
+
+/*
+
+Explanation Line By Line :
+
+1) Import : It is a keyword which is use to import (load) any required packages.
+
+2) java. : It is a rout package of java which contains all sub packages in it.
+
+3) io. : It is a subpackage which contains all related classes which are required for input, output.
+
+4) * : It is a simbol from regular expression which indicates we have to import all contains.
+
+5) class : It is a keyword which is use to declare the class.
+
+6) Demo : It's Name Of The Class, Name of class should be capital letters, 
+          The name of the class contains main function in it.
+
+7) Public : Its an access specifier of java, main function get call by the JVM automatically.
+            JVM is the outsider for our program due to which main should be inside public section.
+
+8) Static : Main function should be declared as static because JVM can call the main function without 
+            creating the object of a class.
+
+9) Void : The return value of main is void because JVM is not interested in the exit status of the program.
+
+10) Main : It is a name allocated by java for the entry point function like c and c++.
+
+11) String : It is a name of class which represent string, string is defined as array of characters
+
+12) arg[] : Its an array which holds command line arguments.
+
+13) System. : It is name of class from the default package of java.
+
+14) out. : It is the name of object which is use to perform the output activity.
+
+15) println : This method is use to perform the printing activity on console (monitor). 
+                ln stands for new line which indicates our function at the newline implesitely.
+
+16) Hello : It is a string which is written as a parameter for println function. 
+
+*/
