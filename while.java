afc48b83d5b3ba0472d1eb373f1d827d0e26d893
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-13 17:42:17 +0530

    Use Of While Loop In Java

---
+++ while.java
@@ -0,0 +1,21 @@
+/*
+
+Description      :  Use Of While Loop In Java 
+Function Date    :  13 Feb 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+
+*/
+
+class While1
+{
+    public static void main(String args[])
+    {
+        int i = 10;
+        while(i > 0)
+        {
+            System.out.println("Tick"+" "+i);
+            i--;
+        }
+    }
+}
