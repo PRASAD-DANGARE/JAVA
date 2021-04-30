ae1d45efac2224ffa71ff5ece5f1a786d907f28a
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-04-30 22:04:20 +0530

    Display Numbers From 1 To 5 Using while Loop

---
+++ Control_statement3.java
@@ -0,0 +1,25 @@
+/*
+
+Description      :  Display Numbers From 1 To 5 Using while Loop
+Class Name       :  Numbers
+Function Author  :  Prasad Dangare
+Input / Output   :  Integer
+Function Date    :  29 April 2021
+
+*/
+
+import java.io.*;
+
+class Numbers
+{
+    public static void main(String args[])
+    {
+        int x = 1;
+        
+        while(x <= 5)
+        {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
