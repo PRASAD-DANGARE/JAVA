4e30e12c59668b4d690aa85f2f5e0c006715b8f2
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-05-01 21:19:04 +0530

    Display Numbers In Descending / Reverse Order

---
+++ No_Reverse.java
@@ -0,0 +1,24 @@
+/*
+
+Description      :  Display Numbers In Descending / Reverse Order
+Class Name       :  Reverse
+Function Author  :  Prasad Dangare
+Input / Output   :  Integer
+Function Date    :  01 May 2021
+
+*/
+
+import java.io.*;
+
+class Reverse
+{
+    public static void main(String args[])
+    {
+        int i = 0;
+
+        for(i = 10; i >= 1; i--)
+        {
+            System.out.println(i + " ");
+        }
+    }
+}
