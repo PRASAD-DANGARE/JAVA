68b7954ff0a710a6c5514d45d740cdc33268bc2c
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-05-02 16:11:41 +0530

    Creating 1-D Array As Static

---
+++ 1d_array2.java
@@ -0,0 +1,23 @@
+/*
+
+Description      :  Creating 1-D Array As Static
+Class Name       :  Arr1
+Function Author  :  Prasad Dangare
+Input / Output   :  Integer
+Function Date    :  02 May 2021
+
+*/
+
+import java.io.*;
+
+class Arr1
+{
+    public static void main(String args[])
+    {
+        int arr[] = {50,60,55,67,70};
+        for(int i = 0; i < 5; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
