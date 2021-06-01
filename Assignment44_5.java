3ffdf8d44beaec3c994398995b890b39d455573c
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-06-01 17:06:14 +0530

    AcceptN No &Display All Elements Which Are * Of 11

---
+++ Assignment44_5.java
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////
+//
+// Description   :  Accept N Numbers From User And Display All Such Elements Which Are Multiples Of 11
+// Input         :  Int
+// Output        :  Int
+// Author        :  Prasad Dangare
+// Date          :  31 May 2021
+//
+///////////////////////////////////////////////////////////
+
+
+import java.util.*;
+
+class Array
+{
+    public void Display(int Arr[])
+    {
+        System.out.println("Elements multiples of 11 Are : ");
+        for(int i = 0; i < Arr.length; i++)
+        {
+            if(Arr[i] % 11 == 0)
+            {
+                System.out.println(Arr[i]);
+            }
+        }
+    }
+}
+
+class Assignment44_5
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        System.out.println("Enter Number Of Elements : ");
+        int iSize = sobj.nextInt();
+
+        int Arr[] = new int[iSize];
+        System.out.println("Enter The Elements : ");
+
+        for(int i = 0; i < Arr.length; i++)
+        {
+            Arr[i] = sobj.nextInt();
+        }
+
+        Array aobj = new Array();
+        aobj.Display(Arr);
+    }
+}
