d5d4e83e7c8163a672567c87b9f5352459060bd2
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-06-09 13:09:02 +0530

    Accept String From User And Display Below Pattern

---
+++ Assignment55_3.java
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////
+//
+// Description   :  Accept String From User And Display Below Pattern
+// Input         :  String
+// Output        :  String
+// Author        :  Prasad Dangare
+// Date          :  4 June 2021
+//
+///////////////////////////////////////////////////////////
+
+/*
+
+Input : Hello
+
+Output : H
+         H e
+         H e l
+         H e l l
+         H e l l o
+		 
+*/
+
+import java.util.*;
+
+class Pattern
+{
+	public void Pattern(String str)
+	{
+		char arr[] = str.toCharArray();
+
+        System.out.println();
+        for(int i = 0; i < arr.length; i++)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                System.out.print("\t" + arr[j]);
+            }
+            System.out.println();
+        }
+	}
+}
+
+class Assignment55_3
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj = new Scanner(System.in);
+        System.out.println("Enter The String : ");
+        String schar = sobj.nextLine();
+
+        Pattern pobj = new Pattern();
+        pobj.Pattern(schar);
+	}
+}
